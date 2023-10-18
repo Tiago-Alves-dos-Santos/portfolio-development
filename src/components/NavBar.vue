ecb9d96f7b79340526813186bf10e49d63498f2d
Tiago Alves dos Santos de Oliveira <tiagooliveiraaso2@gmail.com>
2023-10-18 14:27:14 -0300

    files for navbar

---
+++ src/components/NavBar.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        {{ teste }}
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            teste:'teetet'
+        }
+    },
+}
+</script>
---
+++ src/views/layout/NavFlowLayout.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <NavBar></NavBar>
+        <h1 class="bg-blue-200">layout</h1>
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+import NavBar from '@/components/NavBar.vue';
+export default {
+    components : {
+        NavBar
+    }
+}
+</script>
