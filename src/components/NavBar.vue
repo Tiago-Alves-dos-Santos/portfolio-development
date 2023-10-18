ecf92df67f3b0e2d2399b62239c8a85bd7d60595
Tiago Alves dos Santos de Oliveira <tiagooliveiraaso2@gmail.com>
2023-10-18 17:03:24 -0300

    class active navbar link

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -2,19 +2,20 @@
     <div class="flex justify-center bg-white shadow-md">
         <div class="container flex justify-between ">
             <!-- button toggle dark theme -->
-            <div class="relative top-[10px] rounded-full w-[30px] h-[30px] flex justify-center flex-col items-center bg-black">
+            <div class="relative top-[10px] w-[30px] h-[30px] rounded-full flex justify-center flex-col items-center bg-black
+            cursor-pointer">
                 <font-awesome-icon icon="moon" class="text-yellow-300"/>
                 <font-awesome-icon icon="fa-solid fa-sun" class="hidden"/>
             </div>
             <!-- links of navbar -->
-            <div class="flex space-x-4 ">
+            <div class="flex">
                 <!-- <router-link to="/">SOBRE</router-link> 
                 <router-link to="/">HABILIDADES</router-link> 
                 <router-link to="/">PROJETOS</router-link> 
                 <router-link to="/">EXPERIÊNCIAS</router-link> 
                 <router-link to="/">CONTATO</router-link>  -->
 
-                <a href="" class="py-4 px-4 hover:bg-primary-900 hover:text-white">SOBRE</a>
+                <a href="" class="nav-active py-4 px-4 hover:bg-primary-900 hover:text-white">SOBRE</a>
                 <a href="" class="py-4 px-4 hover:bg-primary-900 hover:text-white">HABILIDADES</a>
                 <a href="" class="py-4 px-4 hover:bg-primary-900 hover:text-white">PROJETOS</a>
                 <a href="" class="py-4 px-4 hover:bg-primary-900 hover:text-white">EXPERIÊNCIAS</a>
