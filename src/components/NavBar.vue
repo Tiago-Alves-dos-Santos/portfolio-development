abe28798a831d3ed397011707eaf239f47c03723
Tiago Alves dos Santos de Oliveira <tiagooliveiraaso2@gmail.com>
2023-10-18 17:24:07 -0300

    responsive navbar tweaks

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="flex justify-center bg-white shadow-md">
-        <div class="container flex justify-between ">
+        <div class="container flex justify-between py-2 md:py-0">
             <!-- button toggle dark theme -->
-            <div class="relative top-[10px] w-[30px] h-[30px] rounded-full flex justify-center flex-col items-center bg-black
-            cursor-pointer">
+            <div class="relative w-[30px] h-[30px] rounded-full flex justify-center flex-col items-center bg-black
+            cursor-pointer md:top-[10px]">
                 <font-awesome-icon icon="moon" class="text-yellow-300"/>
                 <font-awesome-icon icon="fa-solid fa-sun" class="hidden"/>
             </div>
@@ -15,11 +15,13 @@
                 <router-link to="/">EXPERIÊNCIAS</router-link> 
                 <router-link to="/">CONTATO</router-link>  -->
 
-                <a href="" class="nav-active py-4 px-4 hover:bg-primary-900 hover:text-white">SOBRE</a>
-                <a href="" class="py-4 px-4 hover:bg-primary-900 hover:text-white">HABILIDADES</a>
-                <a href="" class="py-4 px-4 hover:bg-primary-900 hover:text-white">PROJETOS</a>
-                <a href="" class="py-4 px-4 hover:bg-primary-900 hover:text-white">EXPERIÊNCIAS</a>
-                <a href="" class="py-4 px-4 hover:bg-primary-900 hover:text-white">CONTATO</a>
+                <div class="hidden md:flex">
+                    <a href="" class="nav-active py-4 px-4 hover:bg-primary-900 hover:text-white">SOBRE</a>
+                    <a href="" class="py-4 px-4 hover:bg-primary-900 hover:text-white">HABILIDADES</a>
+                    <a href="" class="py-4 px-4 hover:bg-primary-900 hover:text-white">PROJETOS</a>
+                    <a href="" class="py-4 px-4 hover:bg-primary-900 hover:text-white">EXPERIÊNCIAS</a>
+                    <a href="" class="py-4 px-4 hover:bg-primary-900 hover:text-white">CONTATO</a>
+                </div>
             </div>
         </div>
     </div>
