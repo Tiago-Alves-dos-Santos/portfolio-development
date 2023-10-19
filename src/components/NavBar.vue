a0beacb4f5bcc9f267fa997d9f1d4e380b440d28
Tiago Alves dos Santos de Oliveira <tiagooliveiraaso2@gmail.com>
2023-10-19 17:51:40 -0300

    hamburger menu added

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,11 +1,16 @@
 <template>
     <div class="flex justify-center bg-white shadow-md dark:bg-black">
-        <div class="container flex justify-between py-2 md:py-0">
+        <div class="container flex justify-between py-2 sm:py-0">
             <!-- button toggle dark theme -->
-            <div id="theme_btn" class="relative w-[30px] h-[30px] rounded-full flex justify-center flex-col items-center  
-            md:top-[10px] border-2 border-black cursor-pointer" @click="toggleTheme">
-                <font-awesome-icon id="theme_dark" icon="moon" class=""/>
-                <font-awesome-icon id="theme_light" icon="fa-solid fa-sun" class="hidden"/>
+            <div class="flex justify-center flex-col">
+                <div class="relative w-[30px] h-[30px] rounded-full flex justify-center flex-col items-center
+                ml-1 
+              dark:border-white
+                sm:ml-0
+                border-2 border-black cursor-pointer" @click="toggleTheme">
+                    <font-awesome-icon id="theme_dark" icon="moon" class=" dark:text-white" />
+                    <font-awesome-icon id="theme_light" icon="fa-solid fa-sun" class="hidden dark:text-white" />
+                </div>
             </div>
             <!-- links of navbar -->
             <div class="flex">
@@ -14,13 +19,30 @@
                 <router-link to="/">PROJETOS</router-link> 
                 <router-link to="/">EXPERIÊNCIAS</router-link> 
                 <router-link to="/">CONTATO</router-link>  -->
-
-                <div class="hidden md:flex">
-                    <a href="" class="nav-active py-4 px-4 hover:bg-primary-900 hover:text-white">SOBRE</a>
-                    <a href="" class="py-4 px-4 hover:bg-primary-900 hover:text-white">HABILIDADES</a>
-                    <a href="" class="py-4 px-4 hover:bg-primary-900 hover:text-white">PROJETOS</a>
-                    <a href="" class="py-4 px-4 hover:bg-primary-900 hover:text-white">EXPERIÊNCIAS</a>
-                    <a href="" class="py-4 px-4 hover:bg-primary-900 hover:text-white">CONTATO</a>
+                <div class="flex flex-col justify-center items-center content-center sm:hidden">
+                    <font-awesome-icon icon="fa-solid fa-bars" class="text-xl mr-2 dark:text-white sm:mr-0" />
+                </div>
+                <div class="hidden sm:flex">
+                    <a href="" class="nav-active py-4 px-4 hover:text-white dark:text-white 
+                    dark:hover:text-black dark:hover:bg-white">
+                        SOBRE
+                    </a>
+                    <a href="" class="py-4 px-4 hover:bg-primary-900 hover:text-white 
+                    dark:text-white dark:hover:text-black dark:hover:bg-white">
+                        HABILIDADES
+                    </a>
+                    <a href="" class="py-4 px-4 hover:bg-primary-900 hover:text-white 
+                    dark:text-white dark:hover:text-black dark:hover:bg-white">
+                        PROJETOS
+                    </a>
+                    <a href="" class="py-4 px-4 hover:bg-primary-900 hover:text-white 
+                    dark:text-white dark:hover:text-black dark:hover:bg-white">
+                        EXPERIÊNCIAS
+                    </a>
+                    <a href="" class="py-4 px-4 hover:bg-primary-900 hover:text-white 
+                    dark:text-white dark:hover:text-black dark:hover:bg-white">
+                        CONTATO
+                    </a>
                 </div>
             </div>
         </div>
@@ -37,17 +59,17 @@ export default {
         }
     },
     methods: {
-        toggleTheme(){
+        toggleTheme() {
             let body = document.body;
             let theme_dark = document.getElementById('theme_dark');
             let theme_light = document.getElementById('theme_light');
-            if(this.theme_actual == Theme.light){//theme to dark
+            if (this.theme_actual == Theme.light) {//theme to dark
                 this.theme_actual = Theme.dark;
                 //class
                 body.classList.add('dark');
                 theme_dark.classList.add('hidden');
                 theme_light.classList.remove('hidden');
-            }else{//theme to light
+            } else {//theme to light
                 this.theme_actual = Theme.light;
                 body.classList.remove('dark');
                 theme_dark.classList.remove('hidden');
--- src/js/font_awessome.js
+++ src/js/font_awessome.js
@@ -1,13 +1,11 @@
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 /* import specific icons */
-import {
-    faMoon,faSun
-} from '@fortawesome/free-solid-svg-icons'
+import * as fontAwessomeSolid from '@fortawesome/free-solid-svg-icons';
 
 /* add icons to the library */
 library.add(
-    faMoon,faSun
+    fontAwessomeSolid.faMoon, fontAwessomeSolid.faSun, fontAwessomeSolid.faBars
 );
 
 export default FontAwesomeIcon
