bb5bc7473dd3cdeef0618c923f93add60f4f5186
Tiago Alves dos Santos de Oliveira <tiagooliveiraaso2@gmail.com>
2023-10-23 15:34:39 -0300

    skills organized

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,10 +1,10 @@
 <template>
-    <div class="w-[200px] bg-white flex justify-center flex-col items-center rounded-md">
+    <div class="w-[200px] bg-white flex justify-center flex-col items-center rounded-md py-2 px-2 m-2 hover:scale-105">
         <div>
-            <img src="@/assets/skills/js.png" alt="" class="w-full"> 
+            <img :src="img" alt="" class="w-[100px]"> 
         </div>
         <div>
-            <h3 class="text-3xl uppercase">JavaScript</h3>
+            <h3 class="text-xl font-bold uppercase">{{title}}</h3>
         </div>
     </div>
 </template>
@@ -13,6 +13,12 @@
 </style>
 <script>
 export default {
-    
+    props: {
+        img: String,
+        title: {
+            type: String,
+            required: true,
+        }
+    },
 }
 </script>
--- src/components/pages/SkillPage.vue
+++ src/components/pages/SkillPage.vue
@@ -3,10 +3,10 @@
         <h1>Skills</h1>
         <div class="container">
             <title-section title="HABILIDADES"></title-section>
-            <div>
-                <card></card>
+            <div class="flex flex-wrap justify-center md:justify-start">
+                <card v-for="(value, index) in skills" :key="index" :title="value.title" :img="value.picture"></card>
             </div>
-        </div>      
+        </div>
     </div>
 </template>
 
@@ -14,12 +14,21 @@
 export default {
     data() {
         return {
-           skills: {} 
+            skills: {},
+        }
+    },
+    methods: {
+        loadSkills() {
+            let habilitys = require("@/assets/skills/habilitys.json");
+            this.skills = habilitys;
+            for (const value of this.skills) {
+                console.log(value.name);
+                value.picture = require("@/assets/skills/" + value.picture);
+            }
         }
     },
     mounted() {
-        let habilitys = require("@/assets/skills/habilitys.json");
-        console.log(habilitys);
+        this.loadSkills();
     }
 }
 </script>
