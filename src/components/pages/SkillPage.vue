38f10e7bf713b94a28ce8f49448dfe7d616c793b
Tiago Alves dos Santos de Oliveira <tiagooliveiraaso2@gmail.com>
2023-10-23 16:12:15 -0300

    select options filter skills

--- src/components/pages/SkillPage.vue
+++ src/components/pages/SkillPage.vue
@@ -1,9 +1,18 @@
 <template>
     <div class="flex justify-center p-4 mt-1 bg-code parallax-bg">
-        <h1>Skills</h1>
         <div class="container">
             <title-section title="HABILIDADES"></title-section>
             <div class="flex flex-wrap justify-center md:justify-start">
+                <div class="flex justify-center w-full mt-2 mb-2">
+                    <select v-model="skillFilter" class="w-1/2 py-3 rounded-lg">
+                        <option value="all">TUDO</option>
+                        <option value="front">FRONT-END</option>
+                        <option value="back">BACK-END</option>
+                        <option value="fr-css">FRAMEWORK CSS</option>
+                        <option value="fr-js">FRAMEWORK JS</option>
+                        <option value="database">DATABASE</option>
+                    </select>
+                </div>
                 <card v-for="(value, index) in skills" :key="index" :title="value.title" :img="value.picture"></card>
             </div>
         </div>
@@ -14,6 +23,7 @@
 export default {
     data() {
         return {
+            skillFilter: 'all',
             skills: {},
         }
     },
