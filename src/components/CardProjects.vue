70c5ec530f5c01572f7ff8c6ba8555b1a2b6f877
Tiago Alves dos Santos de Oliveira <tiagooliveiraaso2@gmail.com>
2023-10-24 14:30:22 -0300

    first layout card projects

--- src/components/CardProjects.vue
+++ src/components/CardProjects.vue
@@ -1,6 +1,20 @@
 <template>
-    <div>
-        card projects
+    <div class="max-w-md py-5 bg-white border rounded-md shadow-md px-7">
+        <div class="flex justify-center">
+            <img src="@/assets/projects/covid.png" alt="">
+        </div>
+        <div>
+            <h1 class="text-4xl text-center">title</h1>
+        </div>
+        <div class="text-justify">
+            is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer too
+        </div>
+        <div class="flex flex-wrap justify-end">
+            <button class="px-3 py-1 mr-2 text-white bg-black rounded-md">GITHUB</button>
+            <button class="px-3 py-1 mr-2 text-white bg-blue-400 rounded-md">LER MAIS</button>
+            <button class="px-3 py-1 mr-2 text-white bg-teal-500 rounded-md">TECNOLOGIAS</button>
+            <button class="px-3 py-1 mr-2 text-white bg-green-500 rounded-md">VER SISTEMA</button>
+        </div>
     </div>
 </template>
 <script>
--- src/components/pages/ProjectsPage.vue
+++ src/components/pages/ProjectsPage.vue
@@ -1,8 +1,8 @@
 <template>
-    <div class="flex justify-center bg-white sm:p-10 dark:bg-primary-100">
+    <div class="flex justify-center p-4 bg-white sm:p-10 dark:bg-primary-100">
         <div class="container">
             <title-section title="PROJETOS"></title-section>
-            <div class="flex justify-center w-full mt-3">
+            <div class="flex justify-center w-full mt-3 md:justify-start">
                 <card-projects></card-projects>
             </div>
         </div>
