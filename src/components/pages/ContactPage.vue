914c85d3b4013f1db14e1ae4e22661432e0581c8
Tiago Alves dos Santos de Oliveira <tiagooliveiraaso2@gmail.com>
2023-10-26 10:45:11 -0300

    links contact

--- src/components/pages/ContactPage.vue
+++ src/components/pages/ContactPage.vue
@@ -2,8 +2,24 @@
     <div class="flex flex-wrap justify-center p-4 bg-white sm:p-10 dark:bg-primary-100 scroll-lazy" id="contatc">
         <div class="container">
             <title-section title="CONTATO"></title-section>
-            <div class="">
-                
+            <div class="flex justify-end w-full px-2 py-3 rounded-md bg-slate-200 md:bg-transparent">
+                <div class="rounded-md md:bg-slate-200 md:p-3">
+                    <a href="https://www.linkedin.com/in/tiago-alves-dos-santos-de-oliveira-96699a189/" target="_blank" class="mr-2 text-2xl text-zinc-600 hover:text-blue-500">
+                        <font-awesome-icon icon="fa-brands fa-linkedin" />
+                    </a>
+                    <a href="https://api.whatsapp.com/send?phone=558899484317&text=Ol%C3%A1,%20cheguei%20aqui%20por%20meio%20de%20seu%20portfolio!%20Me%20chamo..." target="_blank" class="mr-2 text-2xl text-zinc-600 hover:text-green-500">
+                        <font-awesome-icon icon="fa-brands fa-whatsapp" />
+                    </a>
+                    <a href="https://t.me/TiagoAlves2001" target="_blank" class="mr-2 text-2xl text-zinc-600 hover:text-blue-500">
+                        <font-awesome-icon icon="fa-brands fa-telegram" />
+                    </a>
+                    <a href="https://github.com/Tiago-Alves-dos-Santos" target="_blank" class="mr-2 text-2xl text-zinc-600 hover:text-black">
+                        <font-awesome-icon icon="fa-brands fa-github" />
+                    </a>
+                    <a href="" target="_blank" class="text-2xl text-zinc-600 hover:text-red-500">
+                        <font-awesome-icon icon="fa-solid fa-file-pdf" />
+                    </a>
+                </div>
             </div>
             
         </div>
--- src/js/font_awessome.js
+++ src/js/font_awessome.js
@@ -2,10 +2,15 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 /* import specific icons */
 import * as fontAwessomeSolid from '@fortawesome/free-solid-svg-icons';
+import * as fontAwessomeBrand from '@fortawesome/free-brands-svg-icons';
+import * as fontAwessomeRegular from '@fortawesome/free-regular-svg-icons';
 
 /* add icons to the library */
 library.add(
-    fontAwessomeSolid.faMoon, fontAwessomeSolid.faSun, fontAwessomeSolid.faBars
+    fontAwessomeSolid.faMoon, fontAwessomeSolid.faSun, fontAwessomeSolid.faBars, fontAwessomeSolid.faFilePdf,
+    //brands
+    fontAwessomeBrand.faWhatsapp, fontAwessomeBrand.faLinkedin, fontAwessomeBrand.faTelegram, fontAwessomeBrand.faGithub
+    //regular
 );
 
 export default FontAwesomeIcon
