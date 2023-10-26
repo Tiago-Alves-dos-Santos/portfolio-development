5aee7a3b3784dd11b4ed8d41d84fb48373d9c888
Tiago Alves <tiagooliveiraaso2@gmail.com>
2023-10-26 11:59:01 -0300

    url

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   lintOnSave: false,
+  // - /
+  publicPath: '/portfolio/',
   chainWebpack: (config) => {
     config.plugin('html').tap((args) => {
       args[0].title = 'Tiago Alves'; // Altere 'Novo Título' para o título desejado
