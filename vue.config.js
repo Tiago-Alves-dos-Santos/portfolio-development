b30cbf8bbd2e5009136b9135528382f396069b5b
Tiago Alves <tiagooliveiraaso2@gmail.com>
2023-10-26 12:00:57 -0300

    config url

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   lintOnSave: false,
-  // - /
+  // publicPath: process.env.NODE_ENV === 'production' ?'/portfolio/':'/'
   publicPath: '/portfolio/',
   chainWebpack: (config) => {
     config.plugin('html').tap((args) => {
